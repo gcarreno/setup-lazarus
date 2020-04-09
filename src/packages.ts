41ca055bb85e74855f5e1525b9c9427d6be03bb7
Gustavo Carreno <guscarreno@gmail.com>
2020-04-09 21:23:43 +0100

    fix: Always undefined, scope change?

--- src/packages.ts
+++ src/packages.ts
@@ -1,29 +1,29 @@
-const pkgs = {
-  "win32": {
-    "2.0.6": "lazarus-2.0.6-fpc-3.0.4-win32.exe",
-    "2.0.4": "lazarus-2.0.4-fpc-3.0.4-win32.exe"
-  },
-  "linux": {
-    "2.0.6": {
-      "laz": "lazarus-project_2.0.6-0_amd64.deb",
-      "fpc": "fpc-laz_3.0.4-1_amd64.deb",
-      "fpcsrc": "fpc-src_3.0.4-2_amd64.deb"
-    },
-    "2.0.4": {
-      "laz": "lazarus-project_2.0.4-0_amd64.deb",
-      "fpc": "fpc-laz_3.0.4-1_amd64.deb",
-      "fpcsrc": "fpc-src_3.0.4-2_amd64.deb"
-    }
-  }
-}
-
-export function getPackageName(
+export async function getPackageName(
   platform: string,
   lazarusVersion: string,
   pkg: string
 ): string
 {
   let result: string = '';
+
+  const pkgs = {
+    "win32": {
+      "2.0.6": "lazarus-2.0.6-fpc-3.0.4-win32.exe",
+      "2.0.4": "lazarus-2.0.4-fpc-3.0.4-win32.exe"
+    },
+    "linux": {
+      "2.0.6": {
+        "laz": "lazarus-project_2.0.6-0_amd64.deb",
+        "fpc": "fpc-laz_3.0.4-1_amd64.deb",
+        "fpcsrc": "fpc-src_3.0.4-2_amd64.deb"
+      },
+      "2.0.4": {
+        "laz": "lazarus-project_2.0.4-0_amd64.deb",
+        "fpc": "fpc-laz_3.0.4-1_amd64.deb",
+        "fpcsrc": "fpc-src_3.0.4-2_amd64.deb"
+      }
+    }
+  }
 
   switch (platform) {
     case "win32":
