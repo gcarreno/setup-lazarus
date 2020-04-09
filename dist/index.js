e4e247fa6bb264cce056fd5bfdfdf76265469ce0
Gustavo Carreno <guscarreno@gmail.com>
2020-04-09 04:04:12 +0100

    feat: If all goes well, it will download for windows and also install under linux

--- dist/index.js
+++ dist/index.js
@@ -5442,14 +5442,15 @@ __webpack_require__.r(__webpack_exports__);
 //const core = require('@actions/core');
 const tc = __webpack_require__(533);
 const os = __webpack_require__(87);
+const cp = requite('child_process');
 
 const platform = os.platform();
 
 function getLazarus(version) {
   console.log(`getLazarus - Installing Lazarus version:  ${version}`);
 
-  const installDir = tc.find('lazarus', version);
-  console.log(`getLazarus - ${installDir}`);
+  const installDir = tc.find('lazarus');
+  console.log(`getLazarus - Tool Cache install dir: ${installDir}`);
 
   switch (version) {
     case "dist":
@@ -5462,8 +5463,10 @@ function getLazarus(version) {
       }
       break;
     case '2.0.6':
+      downloadLazarus(version, '3.0.4');
+      break;
     case '2.0.4':
-      downloadLazarus(version);
+      downloadLazarus(version, '3.0.4');
       break;
     default:
       throw new Error(`getLazarus - Version not supported: ${lazarusVersion}`);
@@ -5471,15 +5474,34 @@ function getLazarus(version) {
   }
 }
 
-function downloadLazarus(version) {
+function downloadLazarus(versionLaz, versionFPC) {
   console.log(`downloadLazarus - Installing on platform: ${platform}`);
 
   switch (platform) {
     case 'win32':
-      console.log(`downloadLazarus - Downloading Lazarus-${platform}-${version}.exe`);
+      const downloadURL =
+        `https://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2032%20bits/Lazarus%20${version}/lazarus-${versionLaz}-fpc-${versionFPC}-win32.exe`;
+      console.log(`downloadLazarus - Downloading ${downloadURL}`);
+
+      const downloadPath_WIN = tc.downloadTool(downloadURL);
+      console.log(`downloadLazarus - Downloaded into ${downloadPath_WIN}`);
+
       break;
     case 'linux':
-      console.log(`downloadLazarus - Downloading Lazarus-${platform}-${version}`);
+      const downloadLazURL =
+        `https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%20${version}/lazarus-project_${version}-0_amd64.deb`;
+      const downloadFPCURL =
+        `https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%20${version}/fpc-laz_${versionFPC}-1_amd64.deb`;
+      const downloadFPCSRCURL =
+        `https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%20${version}/fpc-src_{versionFPC}-2_amd64.deb`;
+      console.log(`downloadLazarus - Downloading ${downloadLazURL}`);
+
+      const downloadPath_LIN = tc.downloadTool(downloadLazURL);
+      console.log(`downloadLazarus - Downloaded into ${downloadPath_LIN}`);
+
+      const dpkgRes = cp.execSync(`dpkg -i ${downloadPath}`);
+      console.log(`downloadLazarus - Package install result: ${dpkgRes}`);
+
       break;
     default:
       throw new Error(`downloadLazarus - Platform not implemented yet: ${platform}`);
--- src/installer.js
+++ src/installer.js
@@ -1,14 +1,15 @@
 //const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 const os = require('os');
+const cp = requite('child_process');
 
 const platform = os.platform();
 
 export function getLazarus(version) {
   console.log(`getLazarus - Installing Lazarus version:  ${version}`);
 
-  const installDir = tc.find('lazarus', version);
-  console.log(`getLazarus - ${installDir}`);
+  const installDir = tc.find('lazarus');
+  console.log(`getLazarus - Tool Cache install dir: ${installDir}`);
 
   switch (version) {
     case "dist":
@@ -21,8 +22,10 @@ export function getLazarus(version) {
       }
       break;
     case '2.0.6':
+      downloadLazarus(version, '3.0.4');
+      break;
     case '2.0.4':
-      downloadLazarus(version);
+      downloadLazarus(version, '3.0.4');
       break;
     default:
       throw new Error(`getLazarus - Version not supported: ${lazarusVersion}`);
@@ -30,15 +33,34 @@ export function getLazarus(version) {
   }
 }
 
-function downloadLazarus(version) {
+function downloadLazarus(versionLaz, versionFPC) {
   console.log(`downloadLazarus - Installing on platform: ${platform}`);
 
   switch (platform) {
     case 'win32':
-      console.log(`downloadLazarus - Downloading Lazarus-${platform}-${version}.exe`);
+      const downloadURL =
+        `https://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2032%20bits/Lazarus%20${version}/lazarus-${versionLaz}-fpc-${versionFPC}-win32.exe`;
+      console.log(`downloadLazarus - Downloading ${downloadURL}`);
+
+      const downloadPath_WIN = tc.downloadTool(downloadURL);
+      console.log(`downloadLazarus - Downloaded into ${downloadPath_WIN}`);
+
       break;
     case 'linux':
-      console.log(`downloadLazarus - Downloading Lazarus-${platform}-${version}`);
+      const downloadLazURL =
+        `https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%20${version}/lazarus-project_${version}-0_amd64.deb`;
+      const downloadFPCURL =
+        `https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%20${version}/fpc-laz_${versionFPC}-1_amd64.deb`;
+      const downloadFPCSRCURL =
+        `https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%20${version}/fpc-src_{versionFPC}-2_amd64.deb`;
+      console.log(`downloadLazarus - Downloading ${downloadLazURL}`);
+
+      const downloadPath_LIN = tc.downloadTool(downloadLazURL);
+      console.log(`downloadLazarus - Downloaded into ${downloadPath_LIN}`);
+
+      const dpkgRes = cp.execSync(`dpkg -i ${downloadPath}`);
+      console.log(`downloadLazarus - Package install result: ${dpkgRes}`);
+
       break;
     default:
       throw new Error(`downloadLazarus - Platform not implemented yet: ${platform}`);
