11047015ce6cfdd8906d612c7dd3d53dbe9a68f2
Ștefan-Iulian Alecu <165364995+vmaskmovps@users.noreply.github.com>
2024-11-06 21:34:09 +0200

    style: reformat using Prettier

---
+++ src/installer.ts
@@ -0,0 +1,34 @@
+import * as core from "@actions/core";
+import * as laz from "./lazarus";
+import * as pkgs from "./packages";
+
+const RepoBaseURL = "https://packages.lazarus-ide.org";
+const ParamJSON = "packagelist.json";
+
+export class Installer {
+  private _Lazarus: laz.Lazarus;
+  private _IncludePackages: string[];
+  private _Packages: pkgs.Packages;
+
+  constructor(
+    LazarusVerzion: string,
+    PackageList: string[],
+    WithCache: boolean
+  ) {
+    this._Lazarus = new laz.Lazarus(LazarusVerzion, WithCache);
+    this._IncludePackages = PackageList;
+    this._Packages = new pkgs.Packages(LazarusVerzion, RepoBaseURL, ParamJSON);
+  }
+
+  async install(): Promise<void> {
+    core.startGroup("Installing Lazarus");
+    await this._Lazarus.installLazarus();
+    core.endGroup();
+
+    if (this._IncludePackages.length > 0) {
+      core.startGroup("Installing Packages");
+      await this._Packages.installPackages(this._IncludePackages);
+      core.endGroup();
+    }
+  }
+}
